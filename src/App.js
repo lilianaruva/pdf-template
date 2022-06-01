107135b628c6ffef3461c4d7164e1a4515f50fb6
lilianaruva <lily.gallegos@hotmail.com>
2022-06-01 18:13:13 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import PDFViewer from "./components/PDFViewer";
+export default function App() {
+  return (
+    <div className="App">
+      <PDFViewer />
+    </div>
+  );
+}
